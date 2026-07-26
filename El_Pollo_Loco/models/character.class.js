6a5d19656cde84a836cdbd176200bc8978dd3280
Christian Mulugeta <christiantatek.m@gmail.com>
2026-07-26 18:07:27 +0200

    add game world with character, enemies, clouds, and background

---
+++ El_Pollo_Loco/models/character.class.js
@@ -0,0 +1,15 @@
+class Character extends MovableObject {
+
+    y = 130;
+    height = 300;
+    width = 200;
+
+    constructor() {
+        super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
+    }
+
+
+    jump() {
+
+    }
+}
